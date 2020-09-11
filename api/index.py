2b08180e8391c42f00003ea44cadc5176af5af7a
Yang Hanlin <mattoncis@hotmail.com>
2020-09-12 02:13:24 +0800

    fix: fix runtime error

--- api/index.py
+++ api/index.py
@@ -15,7 +15,7 @@ def redirect_for(headers, path):
     scheme = 'https'
     domestic_base = '{scheme}://domestic.pages.tree-diagram.site/{path}'
     global_base = '{scheme}://global.pages.tree-diagram.site/'
-    with geoip2.database.Reader(pathlib.Path(__file__).parent.parent.absolute() + 'db/Country.mmdb') as reader:
+    with geoip2.database.Reader(str(pathlib.Path(__file__).parent.parent.absolute()) + 'db/Country.mmdb') as reader:
         response = reader.country(ip)
         app.logger.info('country or region = %s', response.country.iso_code)
         if response.country.iso_code == 'CN':
