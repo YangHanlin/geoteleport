061b219e622abd97ce40bb282c269e178a9b0416
Yang Hanlin <mattoncis@hotmail.com>
2020-09-12 01:26:36 +0800

    feat: introduce redirection_provider

--- api/index.py
+++ api/index.py
@@ -1,8 +1,12 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, request
+import sys
+
+sys.path.insert(0, '..')
+from lib import redirection_provider
 
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return redirect(f'https://domestic.pages.tree-diagram.site/{path}', code=302)
+    return redirect(redirection_provider.redirect(request.headers, path), code=302)
