9e79f723718cfa1353d797bfcede46b1f310170f
Yang Hanlin <mattoncis@hotmail.com>
2020-09-12 01:51:07 +0800

    refactor: integrate features into one python module

--- api/index.py
+++ api/index.py
@@ -1,10 +1,11 @@
 from flask import Flask, redirect, request
-# from api import redirection_provider
-import reflection_provider
 
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return redirect(redirection_provider.redirect(request.headers, path), code=302)
+    return redirect(redirect_for(request.headers, path), code=302)
+
+def redirect_for(headers, path):
+    return 'https://ip.sb/ip/{ip}'.format(ip=headers.get('x-forwarded-for'))
--- api/redirection_provider.py
+++
@@ -1,4 +0,0 @@
-import geoip2.database
-
-def redirect(headers, path):
-    return 'https://ip.sb/ip/{ip}'.format(ip=headers.get('x-forwarded-for'))
