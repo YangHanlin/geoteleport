2018f570b8d47c6c7b2b9b07978723a31105b4f1
Yang Hanlin <mattoncis@hotmail.com>
2020-09-12 10:25:59 +0800

    refactor: decouple logic and configuration

--- api/index.py
+++ api/index.py
@@ -1,26 +1,34 @@
-from flask import Flask, redirect, request
+from flask import Flask, redirect, abort, request
 import geoip2.database
 import pathlib
+import json
+import os
 
 app = Flask(__name__)
+base_path = str(pathlib.Path(__file__).parent.parent.absolute())
 
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return redirect(redirect_for(request.headers, path), code=302)
+    try:
+        return redirect(redirect_for(request.headers, path), code=302)
+    except RuntimeError as e:
+        abort(400, str(e.args))
 
 
 def redirect_for(headers, path):
     ip = headers.get('x-forwarded-for')
-    app.logger.info('IP = %s', ip)
-    scheme = 'https'
-    domestic_base = '{scheme}://domestic.pages.tree-diagram.site/{path}'
-    global_base = '{scheme}://global.pages.tree-diagram.site/'
-    with geoip2.database.Reader(str(pathlib.Path(__file__).parent.parent.absolute()) + '/db/Country.mmdb') as reader:
-        response = reader.country(ip)
-        app.logger.info('country or region = %s', response.country.iso_code)
-        if response.country.iso_code == 'CN':
-            return domestic_base.format(scheme=scheme, path=path)
-        else:
-            return global_base.format(scheme=scheme, path=path)
+    scheme = headers.get('x-forwarded-proto')
+    config = load_config(base_path + os.getenv('CONFIG_PATH'))
+    rules, fallback = config[:-1], config[-1]
+    with geoip2.database.Reader(base_path + os.getenv('GEOIP_DB_PATH')) as reader:
+        region = reader.country(ip).country.iso_code
+    for rule in rules:
+        if region in rule['regions']:
+            return rule['redirect'].format(scheme=scheme, path=path)
+    return fallback['redirect'].format(scheme=scheme, path=path)
+
+
+def load_config(config_path):
+    return json.load(open(config_path, 'r', encoding='utf-8'))
